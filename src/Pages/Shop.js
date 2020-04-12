3302bd325416a8d1cda6831c302455c30318df45
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2020-04-11 23:16:04 -0700

    basic page

---
+++ src/Pages/Shop.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Shop extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Shop</h1>
+      </div>
+    );
+  }
+}
+
+export default Shop;
