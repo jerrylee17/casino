bacd31b72837945d921699259f3546cf6d0b2821
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2020-04-18 01:32:42 -0700

    everything is up to date

--- src/Pages/Shop/Shop.js
+++ src/Pages/Shop/Shop.js
@@ -3,12 +3,10 @@ import {
   Card, CardText, CardBody, CardTitle, CardSubtitle, CardImg,
   Col,
   Row,
-  Button
-  // Modal,
-  // ModalHeader,
-  // ModalFooter,
-  // ModalBody,
+  Button,
+  Jumbotron
 } from "reactstrap";
+
 import './shop.css';
 
 import ReactDOM from 'react-dom';
@@ -38,39 +36,51 @@ function Shopbadges() {
       badgePurchased: false
     }
   ];
-  // would like to add an image attribute in the future
+
+  // would like to add an image attribute in the future and replace chip balance w/ variable
+
   return (
     <div>
-      <h1>Shop</h1>
+      <Jumbotron>
+      <div className='text-center'>
+        <h1 className='display-4'>Shop</h1>
+      </div>
+    </Jumbotron>
       <h2>Badges of the Day!</h2>
       <section className='user_chip'>
         <b>User's Chip Balance:</b> 5000
       </section>
       <section className='grid'>
         {shopbadges.map((shopbadge, i) => {
-           return ( 
-          <div key={i}>
-            <Col md={10}>
-              <Card>
-                <CardImg top src="https://66.media.tumblr.com/5e6f6e2c27c54517ea7b945919c97a39/tumblr_pfvoq9eW8j1uaogmwo2_250.png" size="100"/>
-                <CardBody>
-                  <CardTitle>{shopbadge.badgeName}</CardTitle>
-                  <CardSubtitle><b>Cost: </b>{shopbadge.badgeCost}</CardSubtitle>
-                  <CardText>
-                    <p>{shopbadge.badgeDesc}</p>
-                    <Row>
-                      <p>{shopbadge.badgePurchased ? "Already Purchased!" : "Not purchased!"}</p>
-                    </Row>
-                  </CardText>
-                </CardBody>
-              </Card>
-            </Col>
-          </div>
-             )
-            })}  
-                </section>
-              
-          </div>
+          return (
+            <div key={i}>
+              <Col md={10}>
+                <Card>
+                  <CardImg top src="https://66.media.tumblr.com/5e6f6e2c27c54517ea7b945919c97a39/tumblr_pfvoq9eW8j1uaogmwo2_250.png" size="100" />
+                  <CardBody>
+                     <CardTitle>{shopbadge.badgeName}</CardTitle>
+                     <Row>
+                       <CardSubtitle><b>Cost: </b>{shopbadge.badgeCost}</CardSubtitle>
+                     </Row>
+                    <CardText>
+                      <Row style={{ height: '100px' }}>
+                        {shopbadge.badgeDesc}
+                      </Row>
+                      <Row>
+                        {shopbadge.badgePurchased ? "Already Purchased!" : "Not purchased!"}
+                      </Row>
+                    </CardText>
+                    <Row><Button>Purchase Badge!</Button></Row>
+                    
+                  </CardBody>
+                </Card>
+              </Col>
+            </div>
+          )
+        })}
+      </section>
+
+    </div>
   );
 }
 // not really sure for shop_no, is it randomized? how is it going to get assigned to the user?
