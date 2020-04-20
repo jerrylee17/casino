b090cb89015ba9efd567e28aa38aa7a82c249b58
Calvin Ly <lycalvin99@gmail.com>
2020-04-20 15:06:55 -0700

    Add logger functionality for errors/requests

--- backend/queries/settings.js
+++ backend/queries/settings.js
@@ -1,9 +1,13 @@
 const connection = require('../connection');
+var logger = require('../logger');
 
 exports.changeUserName = (callback) => {
     let SELECT_ALL_USERS_QUERY = 'SELECT * from users';
     connection.query(SELECT_ALL_USERS_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
         return callback(results);
     });
 }
