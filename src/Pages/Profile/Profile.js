1efdc37232674f9fdae6b5a234d8c8c0401d8670
tient <tientran115@gmail.com>
2020-04-21 15:38:42 -0700

    commit before pulling

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import {Table, Container} from 'reactstrap'
+import {Table, Container, Jumbotron} from 'reactstrap'
 import "./Profile.css"
 import { currentUser } from "../../APIFunctions/user";
 import ProfilePic from '../../Images/profile-pic.png'
@@ -20,22 +20,25 @@ class Profile extends Component {
   render() {
     return (
       <div id="profile-page">
-      <h1 align="center">Profile</h1>
-      <img src={ProfilePic} class="center" width="200" height="180"></img>
+        <Jumbotron className="jumbo">
+          <div className='text-center'>
+            <h1 className='display-4'>Profile</h1>
+          </div>
+          <img src={ProfilePic} class="center" width="200" height="180"></img>
       <p align="center"><b>ID: {this.state.user}</b></p>
       <p align="center"><b>Win/Loss Ratio: 100%</b></p>
       <p align="center"><b>Number of chips: 420</b><img src="https://media.giphy.com/media/13I3peucbA8BfG/giphy.gif" width="30" height="30"></img></p>
-      <p align="center"><b>Status: Normal</b></p>
       <p align="center"><b>Badges:</b></p>
       <section>
         <div class="gallery">
           <div>
-            <img src={Badge1} width="200"></img>
-            <img src={Badge2} width="200"></img>
-            <img src={Badge3} width="200"></img>
+            <img src={Badge1} width="50"></img>
+            <img src={Badge2} width="50"></img>
+            <img src={Badge3} width="50"></img>
           </div>
         </div>
       </section>
+      </Jumbotron>
       <h1 align="center">Game History</h1>
       <Container>
         <Table>
@@ -44,7 +47,9 @@ class Profile extends Component {
              <th>Player</th>
              <th>$$</th>
              <th>Win Rate</th>
-              <th>Type of Game</th>
+             <th>Total Wins</th>
+             <th>Total Games Played</th>
+             <th>Type of Game</th>
             </tr>
           </thead>
         </Table>
