c054f0452601a1cc84d5e9a49b8c5506aecb0ab0
Calvin Ly <lycalvin99@gmail.com>
2020-04-20 20:09:29 -0700

    Add logger functionality for errors/requests (#37)

--- backend/queries/user.js
+++ backend/queries/user.js
@@ -1,9 +1,13 @@
 const connection = require('../connection');
+var logger = require('../logger');
 
 exports.selectAllUsers = (callback) => {
     let SELECT_ALL_USERS_QUERY = 'SELECT * from users';
     connection.query(SELECT_ALL_USERS_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
         return callback(results);
     });
 }
@@ -13,9 +17,16 @@ exports.registerUser = (username, email, hashedPassword, callback) => {
     let REGISTER_QUERY_USER = "INSERT users VALUES ('" + username + "', '" + email + "', '" + hashedPassword + "');";
     let REGISTER_QUERY_PLAYER = "INSERT player VALUES ('" + username + "', 0, 0, '" + currentDate + "', false);";
     connection.query(REGISTER_QUERY_USER, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
         connection.query(REGISTER_QUERY_PLAYER, (err, results) => {
-            if (err) throw err;
+            if (err) {
+                logger.error(err);
+                throw err;
+            };
+            logger.request("registered user - " + username);
             return callback(true);
         });
     });
@@ -24,7 +35,11 @@ exports.registerUser = (username, email, hashedPassword, callback) => {
 exports.checkValidUser = (username, callback) => {
     let FIND_USERNAME_QUERY = "SELECT * FROM users WHERE username='" + username + "'";
     connection.query(FIND_USERNAME_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("checked valid user - " + username);
         return callback(results);
     });
 }
@@ -32,7 +47,11 @@ exports.checkValidUser = (username, callback) => {
 exports.checkValidEmail = (email, callback) => {
     let FIND_EMAIL_QUERY = "SELECT * FROM users WHERE email='" + email + "'";
     connection.query(FIND_EMAIL_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("checked valid email - " + email);
         return callback(results);
     });
 }
@@ -40,7 +59,11 @@ exports.checkValidEmail = (email, callback) => {
 exports.warnUser = (username, callback) => {
     let WARN_USER_QUERY = "UPDATE player SET no_of_warns=no_of_warns+1 WHERE player_id='" + username + "';";
     connection.query(WARN_USER_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("warned user - " + username);
         return callback(results);
     });
 }
@@ -48,7 +71,11 @@ exports.warnUser = (username, callback) => {
 exports.banUser = (username, callback) => {
     let BAN_USER_QUERY = "UPDATE player SET banned=1 WHERE player_id='" + username + "';";
     connection.query(BAN_USER_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("banned user - " + username);
         return callback(results);
     });
 }
@@ -56,7 +83,11 @@ exports.banUser = (username, callback) => {
 exports.unbanUser = (username, callback) => {
     let UNBAN_USER_QUERY = "UPDATE player SET banned=0 WHERE player_id='" + username + "';";
     connection.query(UNBAN_USER_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("unbanned user - " + username);
         return callback(results);
     });
 }
@@ -64,7 +95,11 @@ exports.unbanUser = (username, callback) => {
 exports.checkBanned = (username, callback) => {
     let CHECK_IF_BANNED_USER_QUERY = "SELECT * FROM player WHERE player_id='" + username + "';";
     connection.query(CHECK_IF_BANNED_USER_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("check user banned - " + username);
         return callback(results);
     })
 <<<<<<< HEAD
@@ -76,7 +111,11 @@ exports.checkBanned = (username, callback) => {
 exports.getLastLogin = (username, callback) => {
     let GET_LAST_LOGIN_QUERY = "SELECT last_login FROM player WHERE player_id='" + username + "';";
     connection.query(GET_LAST_LOGIN_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("get last user login - " + username);
         return callback(results);
     });
 }
@@ -85,7 +124,11 @@ exports.updateLastLogin = (username, callback) => {
     let currentDate = new Date().toJSON();
     let UPDATE_LAST_LOGIN_QUERY = "UPDATE player SET last_login='" + currentDate + "' WHERE player_id='" + username + "';";
     connection.query(UPDATE_LAST_LOGIN_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("update last login - " + username);
         return callback(results);
     });
 }
@@ -93,7 +136,11 @@ exports.updateLastLogin = (username, callback) => {
 exports.updateCredit = (username, amount, callback) => {
     let UPDATE_CREDIT_QUERY = "UPDATE player SET no_of_chips=no_of_chips+" + amount + " WHERE player_id='" + username + "';";
     connection.query(UPDATE_CREDIT_QUERY, (err, results) => {
-        if (err) throw err;
+        if (err) {
+            logger.error(err);
+            throw err;
+        };
+        logger.request("update user credit - " + username);
         return callback(results);
     });
 <<<<<<< HEAD
