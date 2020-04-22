e5b021e5b2cf0efa595d1243e3466e888515600e
Calvin Ly <lycalvin99@gmail.com>
2020-04-21 19:18:39 -0700

    Redesigned game dashboard frontend (#46)

--- src/Pages/Games/Games.js
+++ src/Pages/Games/Games.js
@@ -1,147 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
 import {
-  Container,
   Jumbotron,
-  Table
+  Form,
+  FormGroup,
+  Input,
+  Button
 } from "reactstrap";
-import {
-  Link
-} from "react-router-dom";
 import Logo from '../../Images/gameLogo.png';
+import blackjackLogo from '../../Images/blackjackLogo.jpg';
+import coinLogo from '../../Images/coinLogo.png';
+import slotsLogo from '../../Images/slotsLogo.png';
 import "./Games.css"
 
-function Games() {
-  const games = [
-    {
-      id: 1,
-      gameName: 'Google Clubhouse',
-      gameHost: 'google',
-      userCount: '10',
-      botsEnabled: false,
-      link: 'www.google.com'
-    },
-    {
-      id: 2,
-      gameName: 'Youtube Jammers',
-      gameHost: 'youtube',
-      userCount: '3',
-      botsEnabled: true,
-      link: 'www.youtube.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    },
-    {
-      id: 3,
-      gameName: 'U dummy',
-      gameHost: 'dummy391',
-      userCount: '1',
-      botsEnabled: true,
-      link: 'www.facebook.com'
-    }
-  ];
+class Games extends Component {
+  state = {
+    currentGame: "blackjack"
+  }
 
-  return (
-    <div id="games-page">
-      <Jumbotron className="jumbo">
-        <div className='text-center'>
-          <h1 className='display-4'>Games <img src={Logo} class="image" alt="" id="gameLogo"></img></h1>
+  handleGameChange = (e) => {
+    e.preventDefault();
+    this.setState({ currentGame: e.target.value })
+  }
+
+  handleGameImage = () => {
+    let game = this.state.currentGame;
+    if (game === "blackjack") {
+      return (
+        <img src={blackjackLogo} alt="" />
+      )
+    }
+    else if (game === "coinflip") {
+      return (
+        <img src={coinLogo} alt="" />
+      )
+    }
+    else {
+      return (
+        <img src={slotsLogo} alt="" />
+      )
+    }
+  }
+
+  render() {
+    let gameLogo = this.handleGameImage();
+    return (
+      <div id="games-page">
+        <Jumbotron className="jumbo">
+          <div className='text-center'>
+            <h1 className='display-4'>Games <img src={Logo} className="image" alt="" id="gameLogo"></img></h1>
+          </div>
+        </Jumbotron>
+        <div className="games text-center">
+          {gameLogo}
         </div>
-      </Jumbotron>
-      <Container>
-        <Table dark>
-          <thead>
-            <tr>
-              <th>
-                #
-              </th>
-              <th>
-                Name
-              </th>
-              <th>
-                Hosted by
-              </th>
-              <th>
-                Usercount
-              </th>
-              <th>
-                Bots
-              </th>
-              <th>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {games.map((game, i) => {
-              return (
-                <tr key={i}>
-                  <th scope="row">{game.id}</th>
-                  <td>{game.gameName}</td>
-                  <td>{game.gameHost}</td>
-                  <td>{game.userCount}</td>
-                  <td>{game.botsEnabled ? "Enabled" : "Disabled"}</td>
-                  <td><Link to={game.link} className="btn btn-success">Join</Link></td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
-      </Container>
-    </div>
-  );
+        <div className="game-input">
+          <Form id="game-form" inline>
+            <FormGroup>
+              <Input type="select" name="select" id="game-select" onChange={this.handleGameChange}>
+                <option value="blackjack">Blackjack</option>
+                <option value="coinflip">Coin Flip</option>
+                <option value="slots">Slots</option>
+              </Input>
+            </FormGroup>
+            <Button color="success">Play</Button>
+          </Form>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Games;
