e24d1761646ffe7803c4ba912d9ee505766efdc0
Habib <has.sorathia@gmail.com>
2020-04-21 18:40:01 -0700

    Added functionality to delete user

--- src/Components/Settings/DeleteTab.js
+++ src/Components/Settings/DeleteTab.js
@@ -7,31 +7,42 @@ import {
     Input
 } from 'reactstrap';
 import './settings-tab.css';
+import { currentUser, deleteUser } from '../../APIFunctions/user';
 
 class DeleteTab extends Component {
     state = {
-        deleteAccount: false
+        deleteAccount: false,
+        username: currentUser(),
+        password: ''
     }
+    handlePassword = (event) => { this.setState({ password: event.target.value })}
+    handleDelete = (e) => {
+        e.preventDefault();
 
-    handleDelete = () => {
-        // perform account deletion here
+        const user = {
+            username: this.state.username,
+            password: this.state.password
+        }
+        console.log(user);
+        deleteUser(user, result => {
+            if (result === "passwordError") this.setState({ passwordError: true });
+        });
     }
-
     render() {
         return (
             <div id="settings-tab">
-                <Form>
+                <Form onSubmit={this.handleDelete}>
                     {!this.state.deleteAccount ?
                         (
-                            <div>
-                                <h4>Are you sure you want to delete your account?</h4>
-                                <Button className="delete-tab-btn" color="success" onClick={() => { this.setState({ deleteAccount: true }) }}>Yes</Button>
-                            </div>
+                        <div>
+                            <h4>Are you sure you want to delete your account?</h4>
+                            <Button className="delete-tab-btn" color="success" onClick={() => { this.setState({ deleteAccount: true }) }}>Yes</Button>
+                        </div>
                         )
                         :
                         (<FormGroup>
                             <Label>Confirm Your Password</Label>
-                            <Input type="password" name="currentpassword" placeholder="Enter Password Here" required></Input>
+                            <Input type="password" name="currentpassword" value={this.state.password} onChange={event => { this.handlePassword(event) }} required></Input>
                             <Button className="delete-tab-btn" color="danger" onClick={() => this.handleDelete} type="submit">Delete Account</Button>
                         </FormGroup>)
                     }
