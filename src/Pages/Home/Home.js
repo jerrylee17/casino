8c8352c910519215bff2d4e89181b1e281709c1f
Habib Sorathia <36381158+hsorathia@users.noreply.github.com>
2020-04-23 20:09:02 -0700

    Added landing page (#58)

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,23 @@
 import React, { Component } from "react";
 import {
-  Jumbotron, Container, Row, Col
+  Jumbotron, Container, Row, Col,
 } from "reactstrap";
+import { 
+  Link 
+} from "react-router-dom";
 import { currentUser, dailyReward, updateLogin, updateCredit } from "../../APIFunctions/user";
 import "./home.css";
+import Logo from '../../Images/Landing/home.svg';
+import Casino from '../../Images/Landing/casino.svg';
+import Chips from '../../Images/Landing/earningchips.svg';
+import ShoppingCart from '../../Images/Landing/shopping-cart.svg';
+import ShoppingBag from '../../Images/Landing/shopping-bag.svg';
+import ProfileOne from '../../Images/Landing/human.svg';
+import ProfileTwo from '../../Images/Landing/profile.svg';
+import ChangeProfileOne from '../../Images/Landing/human-two.svg';
+import ChangeProfileTwo from '../../Images/Landing/pen.svg';
+
+
 
 class Home extends Component {
   state = {
@@ -31,20 +45,111 @@ class Home extends Component {
       <div id="home-page">
         <Jumbotron>
           <div className='text-center'>
-            <h1 className='display-4'>Blips - Home</h1>
+            <h1 className='display-4'>Home <img src={Logo} alt="" id="homeLogo"></img></h1>
           </div>
         </Jumbotron>
-        <Container>
-          <h1>A home to online gambling</h1>
-          <Row>
-            <h2>Games</h2>
+        <Container className='container-main'>
+          <Row className="rows">
+            <Col className='col-imgs'>
+              <img src={Casino} alt="" className="imgs"></img>
+            </Col>
+            <Col md={6} className='intro-title'>
+              <h1>A home to online gambling</h1>
+              <p>
+                Blips is the best outlet to play games, win chips, and 
+                experience gambling during quarantine.
+              </p>
+              <Link to={'/Games/'} className="btn btn-success">Play Games</Link>
+            </Col>
           </Row>
-          <Row>
-            <h2>Shop</h2>
+          <Row className="rows">
+            <Col md={6} className='col-txt'>
+              <h2>Earning Chips</h2>
+              <div>
+                There are a few ways to earn chips
+                <ul>
+                  <li>
+                    For each game you play, you can bet a certain amount of chips,
+                    if you win, you can take home twice (or more!) 
+                  </li>
+                  <li>
+                    There's a daily login bonus of 200 chips (24 hours after
+                    your previous login))
+                  </li>
+                </ul>
+                <p>Don't forget to spend your chips at the shop!</p>
+              </div>
+            </Col>
+            <Col className='col-imgs'>
+              <img src={Chips} alt="" className="imgs"></img>
+            </Col>
           </Row>
-        
+          <Row className="rows">
+            <Col md={6} className='col-txt'>
+              <h2>Shop <img src={ShoppingCart} alt="" className="imgs-icons"></img>
+              <img src={ShoppingBag} alt="" className="imgs-icons"></img></h2>
+              <p>
+                Spend all your hard earned chips at the shop, where you can buy
+                new badges and deck out your profile.
+              </p>
+              <Link to={'/Shop/'}>Shop</Link>
+            </Col>
+            <Col md={6} className='col-txt'>
+              <h2>Profile <img src={ProfileOne} alt="" className="imgs-icons"></img>
+              <img src={ProfileTwo} alt="" className="imgs-icons"></img></h2>
+              <p>
+                Check out your wins and losses, how many chips you have, and more
+                at your user profile.
+              </p> 
+              <Link to={'/Profile/'}>User Profile</Link>
+            </Col>
+          </Row>
+          <Row className="rows">
+            <Col md={10} className='col-txt'>
+              <h2>User Settings <img src={ChangeProfileOne} alt="" className="imgs-icons"></img>
+                <img src={ChangeProfileTwo} alt="" className="imgs-icons"></img></h2>
+              <p>
+                Head over to the user settings page to change your account details
+                like your username, email, password, and more. Also, feel free to
+                give us feedback here about what we can improve about the website!
+              </p>
+              <Link to={'/Settings/'}>User Settings</Link>
+            </Col>
+          </Row>
         </Container>
+        <div id='contact-us'>
+          <Container id='contact-us'>
+              <Row className='rows-contact'>
+                <Col className='col-contact'>
+                  <p><b>Made by:</b> Team 2 - CMPE 138</p>
+                </Col>
+              </Row>
+              <Row className='rows-contact'>
+                <Col>
+                  <h6>Group members:</h6>
+                </Col>
+                <Col>
+                  <p>Fanus Arefaine</p>
+                </Col>
+                <Col>
+                  <p>Jerry Lee</p>
+                </Col>
+                <Col>
+                  <p>Calvin Ly</p>
+                </Col>
+                <Col>
+                  <p>Tiffany Nguyen</p>
+                </Col>
+                <Col>
+                  <p>Habib Sorathia</p>
+                </Col>
+                <Col>
+                  <p>Tien Tran</p>
+                </Col>
+              </Row>
+          </Container>
         {this.state.dailyReward ? (<p>You have received your daily reward! +500 chips</p>) : <></>}
+        </div>
       </div>
     );
   }
