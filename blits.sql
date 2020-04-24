f8a58fb0e842d862fc4ddabb1fc84f4d37a4e28b
Tiffany Nguyen <tiffany.phuongn@gmail.com>
2020-04-24 15:13:39 -0700

    fsfnsdlkfn

--- blits.sql
+++ blits.sql
@@ -13,6 +13,8 @@ UNIQUE(username, email)
 create table player(
 player_id varchar(15) not null,
 no_of_chips int,
+no_of_wins int,
+no_of_losses int,
 no_of_warns int,
 last_login varchar(50) not null,
 banned varchar(10) DEFAULT '0',
@@ -21,69 +23,55 @@ foreign key(player_id) REFERENCES users(username)
 on update cascade on delete cascade
 );
 
-create table invited_by(
-awarded_chips int not null
-);
--- saw that awarded_chips is 500, but not sure how to do that lmao
--- also stuck on this relationship
-
 create table user_admin(
 admin_id varchar(10) not null,
-primary key(admin_id)
--- foreign key(admin_id) REFERENCES new_user(username)
--- on delete cascade
+primary key(admin_id),
+foreign key(admin_id) REFERENCES users(username)
+on delete cascade
 );
 
 create table user_profile(
 user_id varchar(10) not null,
 no_of_chips int,
 loss_win_ratio int,
-primary key(user_id)
--- foreign key(user_id) REFERENCES new_user(username)
--- on delete cascade
+primary key(user_id),
+foreign key(user_id) REFERENCES users(username)
+on delete cascade
 );
--- don't know how to reference badges in this table
-
-create table plays(
-bet_chips int not null,
-player_id varchar(10) not null,
-game_id varchar(10) not null,
-primary key(player_id, game_id)
--- foreign key(player_id) REFERENCES new_user(username)
--- foreign key(game_id) REFERENCES game(game_no)
-); 
--- relationship between new_user and game
-
-create table players_list(
-game varchar(9),
-max_capacity int not null
--- foreign key(game) REFERENCES game(type)
-); -- i don't think this is complete
 
 create table game(
-game_no varchar(10) not null,
-status varchar(10)
-	check (status in ('Open', 'In Progress', 'Finished')),
+game_no int not null,
+game_status varchar(10)
+	constraint check_status check (game_status in ('Open', 'In Progress', 'Finished')) ENFORCED,
 wager_amt int not null,
-type varchar (9)
-	check(type in ('Blackjack', 'Coin Flip', 'Slots')),
+game_type varchar (9)
+	constraint check_type check(game_type in ('Blackjack', 'Coin Flip', 'Slots')) ENFORCED,
 winner varchar(10) not null,
 primary key(game_no),
--- foreign key(winner) REFERENCES player(player_id)
+constraint game_winner foreign key(winner) REFERENCES player(player_id)
+on update cascade,
 UNIQUE(game_no)
 );
 
-create table shop(
-shop_no int not null,
-primary key(shop_no)
-); -- did not do badges attribute yet
 
-create table use_chips(
-no_of_chips int not null
-); -- relationship between shop and user, don't know how to do derived attributes
+create table bot(
+game_name varchar(10) not null,
+game_id int not null,
+primary key(game_name, game_id),
+foreign key(game_id) REFERENCES game(game_no)
+on update cascade,
+opponent varchar(10) not null,
+foreign key(opponent) REFERENCES users(username)
+on update cascade
+);
 
-create table badges(
-total int, 
+create table badges_shop(
+owner_name varchar(15) not null,
+owned varchar(5) DEFAULT 'false', 
 badge_name varchar(10) not null,
-badge_cost int -- how much the badge costs
-); -- not sure on details for badges
+badge_cost int,
+description varchar(100),
+primary key(badge_name, badge_cost),
+foreign key(owner_name) REFERENCES users(username)
+on update cascade
+); 
