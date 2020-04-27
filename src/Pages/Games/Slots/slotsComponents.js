db7253e8ec278ea728439e903ec2e9ccdfa3308c
Jerry Lee <jerrylee1157@gmail.com>
2020-04-26 18:04:53 -0700

    Slots Frontend (#72)
    * Implemented slots
    
    * Added slots
    
    * made wager button
    
    * Slots frontend done

---
+++ src/Pages/Games/Slots/slotsComponents.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  Jumbotron,
+  Button,
+  Input,
+} from 'reactstrap'
+
+export const Title = (title) => (
+  <Jumbotron>
+    <div className='text-center'>
+      <h1 className='display-4'>{title}</h1>
+    </div>
+  </Jumbotron>
+)
+
+export function UserDashboard(props) {
+  const {
+    startGame,
+    dispComponents,
+    setWager,
+    endGame,
+    winner
+  } = props
+  return (
+    <div>
+      <h1>User panel</h1>
+      <br />
+      <div className='text-center'>
+        <Button
+          color='success'
+          size='lg'
+          hidden={dispComponents}
+          onClick={startGame}>
+          Start game
+        </Button>
+        <Button
+          color='success'
+          size='lg'
+          hidden={!dispComponents}
+          disabled={dispComponents && winner === null}
+          onClick={endGame}>
+          Reset Wager
+        </Button>
+      </div>
+      <br />
+      <Input
+        placeholder='Enter wager. '
+        disabled={dispComponents}
+        onChange={(e) => {
+          setWager(e.target.value)
+        }}
+      />
+    </div>
+  )
+}
